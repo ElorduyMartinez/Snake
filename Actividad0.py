f140768d5ee1f7bac277c9186e46dd73d0e462b2
Alfredo-vqz <a01656384@tec.mx>
2024-03-21 13:49:52 -0600

    La comida se mueve constantemente 1 paso a la vez

--- Actividad0.py
+++ Actividad0.py
@@ -1,26 +1,31 @@
-from random import randrange
 from turtle import *
-
+from random import randrange
 from freegames import square, vector
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
-
 def change(x, y):
-    """Change snake direction."""
+    "Change snake direction."
     aim.x = x
     aim.y = y
 
-
 def inside(head):
-    """Return True if head inside boundaries."""
+    "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
+def move_food():
+    move_x = randrange(-1, 2) * 10
+    move_y = randrange(-1, 2) * 10
+    new_food = vector(food.x + move_x, food.y + move_y)
+
+    if inside(new_food):
+        food.x = new_food.x
+        food.y = new_food.y
 
 def move():
-    """Move snake forward one segment."""
+    "Move snake forward one segment."
     head = snake[-1].copy()
     head.move(aim)
 
@@ -33,11 +38,10 @@ def move():
 
     if head == food:
         print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
     else:
         snake.pop(0)
 
+    move_food()
     clear()
 
     for body in snake:
@@ -46,7 +50,6 @@ def move():
     square(food.x, food.y, 9, 'green')
     update()
     ontimer(move, 100)
-
 
 setup(420, 420, 370, 0)
 hideturtle()
